8815116812165cd166b51cda5cfb62590fefbbf3
HanjalaShihab <shihab2305341402@diu.edu.bd>
2026-01-06 14:07:25 +0600

    Indirect token system

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN") 
 
 # Initialize ML predictor
 ml_predictor = DengueMLPredictor()
